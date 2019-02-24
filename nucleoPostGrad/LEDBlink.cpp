83071c3c9e75fcc82e8806f8031737057c85a33e
ssmith73 <ssmith73@gmail.com>
2019-02-24 19:30:56 +0000

    SW1 LED working - ADC still not working

--- nucleoPostGrad/LEDBlink.cpp
+++ nucleoPostGrad/LEDBlink.cpp
@@ -1,9 +1,10 @@
 #include <stm32l4xx.h>
+#include <stdio.h>
+
 
 #ifdef __cplusplus
 extern "C"
 #endif
-
 
 #define DELAY 500
 void TIM2_IRQHandler(void);
@@ -15,7 +16,7 @@ volatile bool timer2RolledOver = false;
 void configureGpioPorts(void);
 void turn_on_all_leds(void);
 void turn_off_all_leds(void);
-void write_string(uint8_t *);
+void write_string(char *);
 
 void delayMs(int);
 void delay1Hz(void);
@@ -35,6 +36,11 @@ void initUsart(void);
 void initPbInterrupt(void);
 
 void USART2_Write(uint8_t ch);
+
+typedef enum {
+	NORMAL,
+	INVERSE
+}DISPLAYMODES;
 
 typedef enum
 {
@@ -69,21 +75,19 @@ int main(void)
 	DIRECTIONS direction = FORWARD;
 	uint8_t sw1_pressed = 0;
 	uint8_t sw1_pressed_prev = 0;
-	uint8_t sw2_pressed = 0;
-	uint8_t sw2_pressed_prev = 0;
 	uint8_t inverted_cylon = 0;
 	uint32_t adcResult = 0;
 
 	__disable_irq();
 	/* Configure the clocks - using MSI as SYSCLK @16MHz */
-	RCC->CR 		 	&= 	0xFFFFFF07;  //Clear ~MSIRANGE bits and MSIRGSEL bit
-	RCC->CR 		 	|= 	0x00000089;  //Set MSI to 16MHz and MSIRGSEL bit
+	RCC->CR 		 	&= 	0xFFFFFF07;     //Clear ~MSIRANGE bits and MSIRGSEL bit
+	RCC->CR 		 	|= 	0x00000089;     //Set MSI to 16MHz and MSIRGSEL bit
 
-	RCC->AHB2ENR 	|= 	0x00000001;  //Enable PA5 clocks - for on-board LED
+	RCC->AHB2ENR 	|= 	0x00000001;     //Enable PA5 clocks - for on-board LED
 	
 	/* Enable PA0 for output */
-	GPIOA->MODER 	&= 	0xFFFFF3FF;  //Clear GPIOA[5] MODER bits
-	GPIOA->MODER 	|= 	0x00000400;  //Enable GPIOA[5] for output
+	GPIOA->MODER 	&= 	0xFFFFF3FF;     //Clear GPIOA[5] MODER bits
+	GPIOA->MODER 	|= 	0x00000400;     //Enable GPIOA[5] for output
 	
 	/* Configure Timer 2
 	Assumes 16MHz system clock, rolls over
@@ -95,49 +99,39 @@ int main(void)
 	initTim2();
 	initAdc();
 
-	uint8_t str[] = "Led Cylon Display\n";
-	
+	char str[] = "Led Cylon Display\n";
+	DISPLAYMODES display_mode = NORMAL;	
 	__enable_irq();
 	while (1)
 	{
 	
-		//Check switch  state - consider debounce later
-		if (!(GPIOA->IDR & 0x00000020))
+		//Check switch state - consider debounce later
+		if(!(GPIOA->IDR & 0x00000040))
 		{
 			sw1_pressed = 1;
-		//	USART2_Write('5');
 		}
 		else sw1_pressed = 0;
 
-		if (!(GPIOA->IDR & 0x00000040))
-		{
-			sw2_pressed = 1;
-		//	USART2_Write('6');
-		}
-		else sw2_pressed = 0;
 		
 		if (timer2RolledOver == true)
 		{
 
-			ADC1->CR |= 0x00000002;    //Convst
+			ADC1->CR |= 0x00000004;       //Convst
 			while(!(ADC1->ISR & 0x4)) {}
 			adcResult = ADC1->DR;
 			
-
-
-
+			
+			char *dataPtr = NULL;
+			//itoa(adcResult,dataPtr,10);
+			sprintf(dataPtr, "%d", adcResult);
+			write_string(dataPtr);
 			timer2RolledOver = false;
 
 			//Check for a change in the state of a switch
 			if(sw1_pressed_prev != sw1_pressed)
 				next_state = IDLE_STATE;
 
-			if (sw2_pressed_prev != sw2_pressed)
-				next_state = IDLE_STATE;
-
-
 			sw1_pressed_prev = sw1_pressed;
-			sw2_pressed_prev = sw2_pressed;
 
 			/* Basic state machine to handle the LED toggling*/
 
@@ -146,12 +140,17 @@ int main(void)
 			case IDLE_STATE:	
 				next_state = LED2_ON_STATE;
 				if (sw1_pressed) {
-					// inverted cyclon response
-				   inverted_cylon =  sw2_pressed ? 0 : 1;
-					if (sw2_pressed)
-						turn_off_all_leds();
-					else
+					inverted_cylon =  display_mode == INVERSE;
+					if (display_mode == INVERSE)
 						turn_on_all_leds();
+					else
+						turn_off_all_leds();
+
+					if (display_mode == INVERSE)
+						display_mode = NORMAL;
+					else
+						display_mode = INVERSE;
+
 				}
 				else
 					next_state = READ_ADC;
@@ -161,70 +160,70 @@ int main(void)
 								
 			case LED2_ON_STATE: 
 				next_state = LED2_OFF_STATE;
-				GPIOA->BSRR |= inverted_cylon ? 0x04000000 : 0x00000400;  	//Set the GPIO
+				GPIOA->BSRR |= inverted_cylon ? 0x04000000 : 0x00000400;     	//Set the GPIO
 				break;
 			case LED2_OFF_STATE: 
 				next_state =  LED3_ON_STATE;
-				GPIOA->BSRR |= inverted_cylon ? 0x00000400 : 0x04000000;  	//Set the GPIO
+				GPIOA->BSRR |= inverted_cylon ? 0x00000400 : 0x04000000;     	//Set the GPIO
 				direction = FORWARD; 
 				write_string(str);
 				break;
 			case LED3_ON_STATE: 
 				next_state = LED3_OFF_STATE;
-				GPIOB->BSRR |= inverted_cylon ? 0x00080000: 0x00000008;  	//Set the GPIO
+				GPIOB->BSRR |= inverted_cylon ? 0x00080000 : 0x00000008;     	//Set the GPIO
 				break;
 			case LED3_OFF_STATE: 
 				next_state = direction == BACK ? LED2_ON_STATE : LED4_ON_STATE;
-				GPIOB->BSRR |= inverted_cylon  ? 0x00000008 : 0x00080000;    //Clear the GPIO
+				GPIOB->BSRR |= inverted_cylon  ? 0x00000008 : 0x00080000;       //Clear the GPIO
 				break;
 			case LED4_ON_STATE: 
 				next_state = LED4_OFF_STATE;
-				GPIOB->BSRR |= inverted_cylon ? 0x00200000 : 0x00000020;  	//Set the GPIO
+				GPIOB->BSRR |= inverted_cylon ? 0x00200000 : 0x00000020;     	//Set the GPIO
 				break;
 			case LED4_OFF_STATE: 
 				next_state = direction == BACK ? LED3_ON_STATE : LED5_ON_STATE;
-				GPIOB->BSRR |= inverted_cylon ? 0x00000020 : 0x00200000;    //Clear the GPIO
+				GPIOB->BSRR |= inverted_cylon ? 0x00000020 : 0x00200000;       //Clear the GPIO
 				break;
 			case LED5_ON_STATE: 
 				next_state = LED5_OFF_STATE;
-				GPIOB->BSRR |= inverted_cylon ? 0x00100000 : 0x00000010;  	//Set the GPIO
+				GPIOB->BSRR |= inverted_cylon ? 0x00100000 : 0x00000010;     	//Set the GPIO
 				break;
 			case LED5_OFF_STATE: 
 				next_state = direction == BACK ? LED4_ON_STATE : LED6_ON_STATE;
-				GPIOB->BSRR |= inverted_cylon ? 0x00000010 : 0x00100000;    //Clear the GPIO
+				GPIOB->BSRR |= inverted_cylon ? 0x00000010 : 0x00100000;       //Clear the GPIO
 				break;
 			case LED6_ON_STATE: 
 				next_state = LED6_OFF_STATE;
-				GPIOB->BSRR |= inverted_cylon ? 0x04000000 : 0x00000400;  	//Set the GPIO
+				GPIOB->BSRR |= inverted_cylon ? 0x04000000 : 0x00000400;     	//Set the GPIO
 				break;
 			case LED6_OFF_STATE: 
 				next_state = direction == BACK ? LED5_ON_STATE : LED7_ON_STATE;
-				GPIOB->BSRR |= inverted_cylon ? 0x00000400 : 0x04000000;    //Clear the GPIO
+				GPIOB->BSRR |= inverted_cylon ? 0x00000400 : 0x04000000;       //Clear the GPIO
 				break;
 			case LED7_ON_STATE: 
 				next_state = LED7_OFF_STATE;
-				GPIOA->BSRR |= inverted_cylon ? 0x01000000 : 0x00000100;  	//Set the GPIO
+				GPIOA->BSRR |= inverted_cylon ? 0x01000000 : 0x00000100;     	//Set the GPIO
 				break;
 			case LED7_OFF_STATE: 
 				next_state = direction == BACK ? LED6_ON_STATE : LED8_ON_STATE;
-				GPIOA->BSRR |= inverted_cylon ? 0x00000100 : 0x01000000;    //Clear the GPIO
+				GPIOA->BSRR |= inverted_cylon ? 0x00000100 : 0x01000000;       //Clear the GPIO
 				break;
 			case LED8_ON_STATE: 
 				next_state = LED8_OFF_STATE;
-				GPIOC->BSRR |= inverted_cylon ? 0x00020000 : 0x00000002;  	//Set the GPIO
+				GPIOC->BSRR |= inverted_cylon ? 0x00020000 : 0x00000002;     	//Set the GPIO
 				break;
 			case LED8_OFF_STATE: 
 				next_state = direction == BACK ? LED7_ON_STATE : LED9_ON_STATE;
-				GPIOC->BSRR |= inverted_cylon ? 0x00000002 : 0x00020000;    //Clear the GPIO
+				GPIOC->BSRR |= inverted_cylon ? 0x00000002 : 0x00020000;       //Clear the GPIO
 				break;
 			case LED9_ON_STATE: 
 				next_state = LED9_OFF_STATE;
 				direction = BACK;
-				GPIOC->BSRR |= inverted_cylon ? 0x00010000 : 0x00000001;  	//Set the GPIO
+				GPIOC->BSRR |= inverted_cylon ? 0x00010000 : 0x00000001;     	//Set the GPIO
 				break;
 			case LED9_OFF_STATE: 
 				next_state = direction == BACK ? LED8_ON_STATE : LED9_ON_STATE;
-				GPIOC->BSRR |= inverted_cylon ? 0x00000001 : 0x00010000;    //Clear the GPIO
+				GPIOC->BSRR |= inverted_cylon ? 0x00000001 : 0x00010000;       //Clear the GPIO
 				direction = BACK;
 				break;
 				
@@ -242,13 +241,13 @@ void delayMs(int n) {
 	need X * 62.5nS = 0.001, so X= 16000 needs to be the reload value */
 	
 	SysTick->LOAD = 0x3E80 - 1; /*16000 -1 */
-	SysTick->VAL = 0x00000000;  /*Clear the current value register */
-	SysTick->CTRL = 0x5;        /*Enable internal clocks source and enable systick */
-	for (i = 0;i < n;i++) {
+	SysTick->VAL = 0x00000000; /*Clear the current value register */
+	SysTick->CTRL = 0x5; /*Enable internal clocks source and enable systick */
+	for (i = 0; i < n; i++) {
 		while ((SysTick->CTRL & 0x10000) == 0) /* wait for reload */
 		{}
 	}
-	SysTick->CTRL = 0; 					//Stop the timer
+	SysTick->CTRL = 0;    					//Stop the timer
 }
 
 void initTim2() {
@@ -260,12 +259,12 @@ void initTim2() {
 	 * To rollover at 1/4Sec - make reload value 1000/4
 	 **/
 
-	RCC->APB1ENR1	|= 0x1;   		//Enable timer 2
-	TIM2->PSC 		= 16000 - 1;	//Prescalar value - divide 16MHz by 16000
-	TIM2->ARR 		= (1000) / 4 - 1;	//Reload value
-	TIM2->CR1		= 1;			//enable timer
-	TIM2->DIER      |= 1;			//enable interrupt
-	NVIC_EnableIRQ(TIM2_IRQn);		//Enable the interrupt in the NVIC
+	RCC->APB1ENR1	|= 0x1;      		//Enable timer 2
+	TIM2->PSC 		= 16000 - 1;   	//Prescalar value - divide 16MHz by 16000
+	TIM2->ARR 		= (1000) / 4 - 1;   	//Reload value
+	TIM2->CR1		= 1;   			//enable timer
+	TIM2->DIER      |= 1;   			//enable interrupt
+	NVIC_EnableIRQ(TIM2_IRQn);   		//Enable the interrupt in the NVIC
 }
 
 
@@ -276,19 +275,19 @@ void 	delay1Hz() {
 	   10000 to give the 1Hz - 1/16MHz = 62.5ns
 	1600 * 62.5nS = 0.0001S = 100uS, *10000 = 1S
 	*/
-	TIM2->CNT 		= 0; 				//Clear the count register
-	TIM2->CR1 		= 1;  				//Start the counter
+	TIM2->CNT 		= 0;    				//Clear the count register
+	TIM2->CR1 		= 1;     				//Start the counter
 	while(!(TIM2->SR & 1)) {}    		//wait for rollover
-	TIM2->SR &= ~1; 					//Clear UIF bit
-	TIM2->CR1 		= 0;  				//Stop the counter
+	TIM2->SR &= ~1;    					//Clear UIF bit
+	TIM2->CR1 		= 0;     				//Stop the counter
 }
 
 
 
 void initUserSw() {
 	/* Configure switch on the Nucleo board*/
-	RCC->AHB2ENR 	|= 0x4; 			//ENABLE GPIOC port clk
-	GPIOC->MODER 	&= 0xF3FFFFFF; 		//Clear GPIOC[13] for input mode
+	RCC->AHB2ENR 	|= 0x4;    			//ENABLE GPIOC port clk
+	GPIOC->MODER 	&= 0xF3FFFFFF;   		//Clear GPIOC[13] for input mode
 
 	/* Configure the 2 switches on the Arduino shield
 	   The Pushbutton Switches on the board are connected as
@@ -297,10 +296,10 @@ void initUserSw() {
 		Switches are to ground - so need to enable pull-up's on them
 		2-bits per port-bit, 0x1 is pull-up
 	*/
-	RCC->AHB2ENR 	|= 0x1; 			//ENABLE GPIOA port clk
-	GPIOA->MODER 	&= 0xFFFFC3FF; 		//Clear GPIOA[6] & GPIOA[5] for input mode
-	GPIOA->PUPDR	&= 0xFFFFC3FF;      //clear the PA5/6 pupdx bits
-	GPIOA->PUPDR	|= 0x00001400;      //Enable the pull-ups
+	RCC->AHB2ENR 	|= 0x1;    			//ENABLE GPIOA port clk
+	GPIOA->MODER 	&= 0xFFFFCFFF;   		//Clear GPIOA[6] for input mode
+	GPIOA->PUPDR	&= 0xFFFFCFFF;        //clear the PA6 pupdx bits
+	GPIOA->PUPDR	|= 0x00001000;        //Enable the pull-up on PA6
 }
 
 void initUsart() {
@@ -323,76 +322,78 @@ void initUsart() {
 		AF's PA2 - USART2 TX ->AF7, PA3 USART2 RX AF7
      */
 
-	RCC->AHB2ENR 	|= RCC_AHB2RSTR_GPIOARST_Msk;	//Enable the clock for GPIOA
-	RCC->APB1ENR1	|= RCC_APB1ENR1_USART2EN_Msk;	//Enable the USART2 clock
-	GPIOA->MODER 	&= 0xFFFFFF0F; 					//Clear PA2/3 bits
-	GPIOA->MODER 	|= 0x000000A0; 					//Set PA2/3 for AF mode
-	GPIOA->AFR[0]   &= 0xFFFF00FF;					//Clear the AF bits for 2&3
-	GPIOA->AFR[0]   |= 0x00007700;					//Set both AF modes to AF7, for bits 2/3
+	RCC->AHB2ENR 	|= RCC_AHB2RSTR_GPIOARST_Msk;   	//Enable the clock for GPIOA
+	RCC->APB1ENR1	|= RCC_APB1ENR1_USART2EN_Msk;   	//Enable the USART2 clock
+	GPIOA->MODER 	&= 0xFFFFFF0F;    					//Clear PA2/3 bits
+	GPIOA->MODER 	|= 0x000000A0;    					//Set PA2/3 for AF mode
+	GPIOA->AFR[0]   &= 0xFFFF00FF;   					//Clear the AF bits for 2&3
+	GPIOA->AFR[0]   |= 0x00007700;   					//Set both AF modes to AF7, for bits 2/3
 	//USART2->BRR		|= 0x115B;						//Set BAUD Rate to 115200 with UartClk at 16MHz
-	USART2->BRR		&= 0x0000;						//Set BAUD Rate to 115200 with UartClk at 16MHz
-	USART2->BRR		|= 0x008B;						//Set BAUD Rate to 115200 with UartClk at 16MHz
-	USART2->CR1     &= 0xEFFF6FFE;					//Clear the M1,OVER8,M0 bits, set 1 start-bit, 8-data bits n stop-bits, keep UE low
-	USART2->CR2     &= 0xFFFFC000;					//Clear the stop-bits to give 1 stop-bit (default anyway)
-	USART2->CR1     |= 0x00000009;					//Enable the TX and the uart
+	USART2->BRR		&= 0x0000;   						//Set BAUD Rate to 115200 with UartClk at 16MHz
+	USART2->BRR		|= 0x008B;   						//Set BAUD Rate to 115200 with UartClk at 16MHz
+	USART2->CR1     &= 0xEFFF6FFE;   					//Clear the M1,OVER8,M0 bits, set 1 start-bit, 8-data bits n stop-bits, keep UE low
+	USART2->CR2     &= 0xFFFFC000;   					//Clear the stop-bits to give 1 stop-bit (default anyway)
+	USART2->CR1     |= 0x00000009;   					//Enable the TX and the uart
 
 }
 
 void initPbInterrupt() {
 	
-	RCC->APB2ENR        |= 1; 			//Enable SYSCFG clk (for GPIO interrupt enables)
-	SYSCFG->EXTICR[3] 	&= ~0x00F0; 	//CLEAR_BIT the EXTI[13] bits
-	SYSCFG->EXTICR[3]   |= 0x20; 		//Enable GPIOC for EXTI[13]
-	EXTI->IMR1 			|= 0x2000; 	//Unmask EXTI13
-	EXTI->FTSR1 		|= 0x2000;  //Enable falling edge triggered interrupts (pushbutton high to low on push)
-	NVIC_EnableIRQ(EXTI15_10_IRQn); //Enable EXTI15-to-10 interrupts
+	RCC->APB2ENR        |= 1;    			//Enable SYSCFG clk (for GPIO interrupt enables)
+	SYSCFG->EXTICR[3] 	&= ~0x00F0;    	//CLEAR_BIT the EXTI[13] bits
+	SYSCFG->EXTICR[3]   |= 0x20;    		//Enable GPIOC for EXTI[13]
+	EXTI->IMR1 			|= 0x2000;    	//Unmask EXTI13
+	EXTI->FTSR1 		|= 0x2000;     //Enable falling edge triggered interrupts (pushbutton high to low on push)
+	NVIC_EnableIRQ(EXTI15_10_IRQn);    //Enable EXTI15-to-10 interrupts
 }
 	 
 void initAdc(void)
 {
-	RCC->AHB2ENR |= 0x20000000; //Enable the ADC clock
-	GPIOA->ASCR  |= 0x00000001; //Connect analog switch to GPIOA[0]
-	GPIOA->MODER |= 0x00000003; //Set A0 for analog input mode
-	ADC1->SQR1   |= 0x00000000; //Set for a sequence of 1 conversion on CH0
-	ADC1->CR	 |= 0x00000001; //Enable ADC1
+	RCC->AHB2ENR |= 0x00002000;    //Enable the ADC clock
+	GPIOA->ASCR  |= 0x00000001;    //Connect analog switch to GPIOA[0]
+	GPIOA->MODER |= 0x00000003;    //Set A0 for analog input mode
+	ADC1->ISR    |= 0x00000001;    //Clear the ADRDY bit in the ADCx_ISR register by writing ‘1’.
+	ADC1->SQR1   |= 0x00000040;    //Set for a sequence of 1 conversion on CH0
+	ADC1->CR	 &= 0xDFFFFFFF;    //Take ADC out of deep power down
+	ADC1->CR	 |= 0x00000001;    //Enable ADC1
 }
 
 void turn_on_all_leds(void)
 {
-/*
-	LED2 - D2 - PA10
-	LED3 - D3 - PB3
-	LED4 - D4 - PB5
-	LED5 - D5 - PB4
-	LED6 - D6 - PB10
-	LED7 - D7 - PA8
-	LED8 - A5 - PC1
-	LED9 - A4 - PC0
- */
+	/*
+		LED2 - D2 - PA10
+		LED3 - D3 - PB3
+		LED4 - D4 - PB5
+		LED5 - D5 - PB4
+		LED6 - D6 - PB10
+		LED7 - D7 - PA8
+		LED8 - A5 - PC1
+		LED9 - A4 - PC0
+	 */
 
-	GPIOA->BSRR |= 0x00000500;  	//Set LED2/7
-	GPIOB->BSRR |= 0x00000438;  	//Set LED3/4/5/6
-	GPIOC->BSRR |= 0x00000003;  	//Set LED8/9
+	GPIOA->BSRR |= 0x00000500;     	//Set LED2/7
+	GPIOB->BSRR |= 0x00000438;     	//Set LED3/4/5/6
+	GPIOC->BSRR |= 0x00000003;     	//Set LED8/9
 
 }
 
 void turn_off_all_leds(void)
 {
-/*
-	LED2 - D2 - PA10
-	LED3 - D3 - PB3
-	LED4 - D4 - PB5
-	LED5 - D5 - PB4
-	LED6 - D6 - PB10
-	LED7 - D7 - PA8
-	LED8 - A5 - PC1
-	LED9 - A4 - PC0
- */
+	/*
+		LED2 - D2 - PA10
+		LED3 - D3 - PB3
+		LED4 - D4 - PB5
+		LED5 - D5 - PB4
+		LED6 - D6 - PB10
+		LED7 - D7 - PA8
+		LED8 - A5 - PC1
+		LED9 - A4 - PC0
+	 */
 
 
-	GPIOA->BSRR |= 0x05000000;  	//Set LED2/7
-	GPIOB->BSRR |= 0x04380000;  	//Set LED3/4/5/6
-	GPIOC->BSRR |= 0x00030000;   	//Set LED8/9
+	GPIOA->BSRR |= 0x05000000;     	//Set LED2/7
+	GPIOB->BSRR |= 0x04380000;     	//Set LED3/4/5/6
+	GPIOC->BSRR |= 0x00030000;      	//Set LED8/9
 
 }
 
@@ -412,21 +413,21 @@ void configureGpioPorts()
 	LED9 - A4 - PC0
 	*/
 
-	RCC->AHB2ENR 	|= 	0x00000007;  //Enable PA,B,C clocks
+	RCC->AHB2ENR 	|= 	0x00000007;     //Enable PA,B,C clocks
 	
 	/* Enable PA8, PA10 for output */
-	GPIOA->MODER 	&= 	0xFFCCFFFF;  //Clear GPIOA[10,8] MODER bits
-	GPIOA->MODER 	|= 	0x00110000;  //Enable GPIOA[10,8] MODER  bits for output
+	GPIOA->MODER 	&= 	0xFFCCFFFF;     //Clear GPIOA[10,8] MODER bits
+	GPIOA->MODER 	|= 	0x00110000;     //Enable GPIOA[10,8] MODER  bits for output
 	/* Enable PA3,PB4,PB5,PB10 for output */
-	GPIOB->MODER 	&= 	0xFFCFF03F;  //Clear GPIOB[10,5,4,3] MODER bits
-	GPIOB->MODER 	|= 	0x00100540;  //Enable GPIOB[10,5,4,3] MODER bits for output
+	GPIOB->MODER 	&= 	0xFFCFF03F;     //Clear GPIOB[10,5,4,3] MODER bits
+	GPIOB->MODER 	|= 	0x00100540;     //Enable GPIOB[10,5,4,3] MODER bits for output
 	/* Enable PC0, PC1 for output */
-	GPIOC->MODER 	&= 	0xFFFFFFF0;  //Clear GPIOC[1,0] MODER bits
-	GPIOC->MODER 	|= 	0x00000005;  //Enable GPIOC[1,0] MODER  bits for output
+	GPIOC->MODER 	&= 	0xFFFFFFF0;     //Clear GPIOC[1,0] MODER bits
+	GPIOC->MODER 	|= 	0x00000005;     //Enable GPIOC[1,0] MODER  bits for output
 
 }
 
-void write_string(uint8_t *str)
+void write_string(char *str)
 {
 	while (*str)
 		USART2_Write(*str++);
